f5ac8e7434202b29900c1792ccf8c609cb7537fe
Drod86 <Daniel.r.rodriguez86@gmail.com>
2020-07-22 08:08:19 -0400

    Changes made led to empty array res from API

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -4,26 +4,18 @@ import * as BooksAPI from './BooksAPI';
 import BookDisplay from './components/book-display';
 
 
-{console.log(BooksAPI.getAll().then(data => {console.log(data)}))}
 
 class App extends Component {
-  state = {
-    books: []
-  }
 
-  componentDidMount() {
-    BooksAPI.getAll()
-    .then(data => {
-      this.setState(curState => ({
-        books: data
-      }))
-    })
-  }
+componentDidMount(){
+	BooksAPI.getAll()
+	.then(res => console.log(res));
+}
 
   render() {
     return (
       <div className="App">
-        <BookDisplay />
+
       </div>
     );
   }
